c9a9db290efecd59c37bc525836b97f0df693763
dragonfore4 <kemkaikub@gmail.com>
2025-11-20 09:37:30 +0700

    feat: update agent import logic to exclude HR_general_information_agent.yaml

--- askHR-workshop/LAB_1_RAG_AGENT/import_all.sh
+++ askHR-workshop/LAB_1_RAG_AGENT/import_all.sh
@@ -7,7 +7,7 @@ for kb_file in "${SCRIPT_DIR}"/group_policy/*.yaml; do
 done
 
 for agent_file in "${SCRIPT_DIR}"/agents/*.yaml; do
-    [ -f "$agent_file" ] && [[ "$agent_file" != *"METRO_general_agent.yaml" ]] && orchestrate agents import -f "$agent_file"
+    [ -f "$agent_file" ] && [[ "$agent_file" != *"HR_general_information_agent.yaml" ]] && orchestrate agents import -f "$agent_file"
 done
 
 # [ -f "${SCRIPT_DIR}/agents/METRO_general_agent.yaml" ] && orchestrate agents import -f "${SCRIPT_DIR}/agents/METRO_general_agent.yaml"
